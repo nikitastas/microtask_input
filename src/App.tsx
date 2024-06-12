1b80d9870df1f58e68417d3f2a7cc715e5447a9c
nikitastas <nikitastas1694@gmail.com>
2024-06-13 00:56:55 +0300

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from './components/FullInput';
 import {Input} from './components/Input';
 import {Button} from './components/Button';
 
